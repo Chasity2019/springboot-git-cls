7ed147a9322c67e98ac5c339292d7bb2ecabd0f9
Chasity2019 <chasity.spruill@gmail.com>
2022-06-16 05:38:56 -0400

    Added practice class

---
+++ src/main/java/edu/cpcc/labs/demo/Practice.java
@@ -0,0 +1,10 @@
+package edu.cpcc.labs.demo;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey world");
+	}
+
+}
