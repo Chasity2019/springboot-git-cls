853cb8961cb28791e9370614850d3b053cbeac95
Chasity2019 <chasity.spruill@gmail.com>
2022-06-16 04:59:14 -0400

    Initial version

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitClsApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitClsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitClsApplication.class, args);
+	}
+
+}
